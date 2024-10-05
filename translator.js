3f38f69d535e13d25c8705ce172fab4b4fb51559
tylertealdev <tyleriteal@proton.me>
2024-10-05 15:45:55 -0400

    Hoosier, Hoosier, can you shut up? Are you an asshole who will never stop?

--- translator.js
+++ translator.js
@@ -1,71 +1,71 @@
 // Basic console log and alert for debugging
-alert("You EaglerPatrol folks will NEVER catch me lacking")
+alert("You EaglerPatrol folks will NEVER catch me lacking");
+
 // Main translation function
-function RaveVM(javaCode) {
-    const lines = javaCode.split('\n');
-    let pythonCode = '';
+function ARVM(pythonCode) {
+    const lines = pythonCode.split('\n');
+    let javaScriptCode = '';
     let indentationLevel = 0;
 
     lines.forEach(line => {
         const trimmedLine = line.trim();
 
-        // Adjust indentation for curly braces
-        if (trimmedLine.endsWith('{')) {
-            pythonCode += '    '.repeat(indentationLevel) + trimmedLine.replace('{', ':') + '\n';
+        // Adjust indentation for colons
+        if (trimmedLine.endsWith(':')) {
+            javaScriptCode += '    '.repeat(indentationLevel) + trimmedLine.slice(0, -1) + ' {' + '\n';
             indentationLevel++;
         } else if (trimmedLine === '}') {
             indentationLevel--;
+            javaScriptCode += '    '.repeat(indentationLevel) + '}' + '\n';
         } else {
             let translatedLine = translateLine(trimmedLine);
-            pythonCode += '    '.repeat(indentationLevel) + translatedLine + '\n';
+            javaScriptCode += '    '.repeat(indentationLevel) + translatedLine + '\n';
         }
     });
 
-    return pythonCode.trim(); // Return trimmed code
+    return javaScriptCode.trim(); // Return trimmed code
 }
 
 // Line translation logic
 function translateLine(line) {
     return line
-        .replace(/public\s+/g, '') // Access modifiers
-        .replace(/private\s+/g, '')
-        .replace(/protected\s+/g, '')
-        .replace(/static\s+/g, '')
-        .replace(/void\s+/g, '')
-        .replace(/System\.out\.println\((.+?)\);/g, 'print($1)') // Print statements
-        .replace(/;\s*$/g, '') // Remove semicolons
-        .replace(/(\w+)\s+(\w+)\s*=\s*(.+?);/g, '$1 $2 = $3') // Variable assignments
-        .replace(/public class/g, 'class') // Class declaration
-        .replace(/main\s*\(String\[\]\s*\w+\)\s*{/g, 'if __name__ == "__main__":') // Main method
-        .replace(/for\s*\((.*?)\)\s*{/g, 'for $1:') // For loops
-        .replace(/while\s*\((.*?)\)\s*{/g, 'while $1:') // While loops
-        .replace(/if\s*\((.*?)\)\s*{/g, 'if $1:') // If statements
-        .replace(/else\s*{/g, 'else:') // Else statements
-        .replace(/else\s+if\s*\((.*?)\)\s*{/g, 'elif $1:') // Else if statements
-        .replace(/return\s+(.+?);/g, 'return $1') // Return statements
-        .replace(/int\s+/g, '') // Remove int keyword
-        .replace(/String\s+/g, '') // Remove String keyword
-        .replace(/boolean\s+/g, 'bool ') // Translate boolean type
-        .replace(/(true|false)\s*;/g, '$1') // Boolean values
-        .replace(/ArrayList<(\w+)>\s+(\w+)\s*=\s*new ArrayList<\1>\(\);/g, '$2 = []') // ArrayList to list
-        .replace(/(\w+)\s+\[\]\s+(\w+)\s*=\s*new (\w+)\[\d+\];/g, '$2 = [$1 for _ in range(0)]') // Array declaration
-        .replace(/this\.(\w+)\s*=\s*(.+?);/g, 'self.$1 = $2') // 'this' handling
-        .replace(/(\w+)\s*\(\s*(.*?)\s*\)/g, 'def $1($2):') // Method declarations
-        .replace(/new\s+(\w+)\s*\(\)/g, '$1()') // Object instantiation
-        .replace(/(\w+)\s*\[(\d+)\]/g, '$1[$2]') // Array access
-        .replace(/break;/g, 'break') // Break statement
-        .replace(/continue;/g, 'continue') // Continue statement
-        .replace(/switch\s*\((\w+)\)\s*{/g, 'switch_var = $1\n    match switch_var:\n') // Switch statement
-        .replace(/case\s+(.+?):/g, '    case $1:') // Case statements
-        .replace(/default:\s*{/g, '    default:') // Default case
-        .replace(/try\s*{/g, 'try:\n    ') // Try block
-        .replace(/catch\s*\((\w+)\s+(\w+)\)\s*{/g, 'except $1 as $2:\n    ') // Catch block
-        .replace(/finally\s*{/g, 'finally:\n    '); // Finally block
+        .replace(/def\s+(\w+)\((.*?)\):/g, 'function $1($2)') // Function declaration
+        .replace(/print\((.+?)\)/g, 'console.log($1)') // Print statements
+        .replace(/([a-zA-Z_]\w*)\s*=\s*([^;]+)/g, '$1 = $2') // Variable assignments
+        .replace(/if\s+(.*?):/g, 'if ($1) {') // If statements
+        .replace(/else:/g, 'else {') // Else statements
+        .replace(/elif\s+(.*?):/g, 'else if ($1) {') // Else if statements
+        .replace(/while\s+(.*?):/g, 'while ($1) {') // While loops
+        .replace(/for\s+(\w+)\s+in\s+(\w+):/g, 'for (let $1 of $2) {') // For loops
+        .replace(/return\s+(.*?)/g, 'return $1;') // Return statements
+        .replace(/True/g, 'true') // Boolean values
+        .replace(/False/g, 'false') // Boolean values
+        .replace(/None/g, 'null') // None to null
+        .replace(/([a-zA-Z_]\w*)\s*=\s*\[\]/g, '$1 = [];') // List initialization
+        .replace(/([a-zA-Z_]\w*)\s*=\s*range\((.*?)\)/g, '$1 = Array.from({ length: $2 }, (_, i) => i);') // Range to array
+        .replace(/(self\.(\w+))\s*=\s*(.+)/g, '$2 = $3') // Handling 'self'
+        .replace(/(\w+)\[(\d+)\]/g, '$1[$2]') // Array access
+        .replace(/break/g, 'break;') // Break statement
+        .replace(/continue/g, 'continue;') // Continue statement
+        .replace(/try:/g, 'try {') // Try block
+        .replace(/except\s+(\w+)\s+as\s+(\w+):/g, 'catch ($2) {') // Catch block
+        .replace(/finally:/g, 'finally {') // Finally block
+        .replace(/class\s+(\w+):/g, 'class $1 {') // Class declaration
+        .replace(/@(\w+)/g, '/* decorator $1 */') // Decorators
+        .replace(/with\s+(.*?)\s+as\s+(\w+):/g, '/* with statement */') // With statement
+        .replace(/([a-zA-Z_]\w*)\s*=\s*\{(.*?)\}/g, '$1 = {$2}') // Dictionary initialization
+        .replace(/for\s+(\w+)\s+in\s+(\w+)\s*:\s*(.*)/g, 'for (let $1 of $2) {$3') // For loop with else
+        .replace(/(\w+)\s*\+\=\s*(.+)/g, '$1 += $2;') // Augmented assignments
+        .replace(/(\w+)\s*-\=\s*(.+)/g, '$1 -= $2;') // Augmented assignments
+        .replace(/(\w+)\s*\*\=\s*(.+)/g, '$1 *= $2;') // Augmented assignments
+        .replace(/(\w+)\s*\/=\s*(.+)/g, '$1 /= $2;') // Augmented assignments
+        .replace(/if\s+(.*?):\s*(.*)/g, 'if ($1) {$2') // Inline if statements
+        .replace(/(\w+)\s*\.\s*(\w+)\((.*?)\)/g, '$1.$2($3)'); // Method calls
 }
 
 // Event listener for button click
 document.getElementById("translateBtn").addEventListener("click", function() {
-    const javaInput = document.getElementById("javaInput").value;
-    const pythonOutput = RaveVM(javaInput);
-    document.getElementById("pythonOutput").textContent = pythonOutput;
+    const pythonInput = document.getElementById("pythonInput").value;
+    const javaScriptOutput = ARVM(pythonInput);
+    document.getElementById("javaScriptOutput").textContent = javaScriptOutput;
 });
